2c3ac22e2adc26e0cd5d0f5d4a10b3731ea75a7c
Leon Biju <31255961+leon-biju@users.noreply.github.com>
2025-12-03 21:09:00 +0000

    websocket connection with rustls

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 mod binance;
 mod snapshot;
+mod stream;
 
 use anyhow::Result;
 
 
 #[tokio::main]
 async fn main() -> Result<()>{
-    println!("Hello, world!");
+    //install default tls provider for aws-lc-rs
+    let _ = rustls::crypto::aws_lc_rs::default_provider().install_default();
     
-    let snapshot = snapshot::fetch_snapshot("BTCUSDT", 1).await?;
-    println!("{:?}", snapshot);
+    let symbol = "BTCUSDT";
+
+
+    println!("Connecting to WebSocket...");
+    let mut ws_stream = stream::connect_depth_stream(symbol).await?;
+    
+    println!("Fetching snapshot...");
+    let snapshot = snapshot::fetch_snapshot(symbol, 1).await?;
+    println!("Snapshot lastUpdateId: {}", snapshot.last_update_id);
+    
+    
+    
+    
+    
     Ok(())
 }
