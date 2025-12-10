a75fa17986db826ef09c6774285779eb52b067dd
Leon Biju <leonbiju25@gmail.com>
2025-12-10 13:57:03 +0000

    add resync logic

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ mod orderbook;
 use anyhow::Result;
 use futures_util::StreamExt;
 
-use crate::binance::DepthUpdate;
-
 
 #[tokio::main]
 async fn main() -> Result<()>{
@@ -35,10 +33,32 @@ async fn main() -> Result<()>{
     println!("Processing deltas!");
     tokio::pin!(ws_stream);
 
+
+    // main listening loop   
     while let Some(result) = ws_stream.next().await {
+
         let update = result?;
-        handle_message(update, &mut sync, &mut book)?;
-        
+
+        match sync.process_delta(update) {
+            sync::SyncOutcome::Updates(updates) => {
+                for update in updates {
+                    println!("Applying update! U={}, u={}", update.first_update_id, update.final_update_id);
+                    book.apply_update(&update);
+                }
+            }
+            sync::SyncOutcome::GapBetweenUpdates => {
+                println!("Gap detected; refetching snapshot and resetting state");
+                let snapshot = snapshot::fetch_snapshot(symbol, 1000).await?;
+                println!("Snapshot lastUpdateId: {}", snapshot.last_update_id);
+                sync = sync::SyncState::new();
+                sync.set_last_update_id(snapshot.last_update_id);
+                book = orderbook::OrderBook::from_snapshot(snapshot);
+            }
+            sync::SyncOutcome::NoUpdates => {
+                println!("No updates!");
+            }
+        }
+
         println!("{:?}", book.top_n_depth(2));
         //break; //TEMPORARY DEBUG STATEMENT to only listen to one message
     }
@@ -48,20 +68,5 @@ async fn main() -> Result<()>{
     
     
     
-    Ok(())
-}
-
-
-
-fn handle_message(update: DepthUpdate, sync: &mut sync::SyncState, book: &mut orderbook::OrderBook) -> Result<()>{
-    let first_id = update.first_update_id;
-    let final_id = update.final_update_id;
-    if let Some(updates) = sync.process_delta(update)? {
-        for update in updates {
-            println!("Applying update! U={}, u={}", first_id, final_id);
-
-            book.apply_update(&update);
-        }
-    }
     Ok(())
 }
