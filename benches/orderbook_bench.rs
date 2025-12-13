76e1ccec3774df0ac146ad5de5a8970721005327
Leon Biju <31255961+leon-biju@users.noreply.github.com>
2025-12-13 16:32:19 +0000

    correct benchmarks: don't use same mut book across updates

--- benches/orderbook_bench.rs
+++ benches/orderbook_bench.rs
@@ -1,21 +1,22 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion, BatchSize};
 use std::hint::black_box;
 use orderbook_engine::{book::orderbook::OrderBook, book::scaler::Scaler, binance::types::{DepthSnapshot, DepthUpdate}};
 use rust_decimal::Decimal;
 use std::str::FromStr;
 
-const SNAPSHOT_LEVELS: usize = 1000;
-const UPDATE_LEVELS: usize = 10;
+const SNAPSHOT_LEVELS: usize = 10000;
+const UPDATES_PER_BATCH: usize = 100;
+const LEVELS_PER_UPDATE: usize = 100;
 
 fn bench_from_snapshot(c: &mut Criterion) {
-    // simulate a snapshot with 10k bids and 10k asks
+    // simulate a snapshot
     let snapshot = DepthSnapshot::fake_snapshot(SNAPSHOT_LEVELS);
     let scaler = Scaler::new(
         Decimal::from_str("0.01").unwrap(),
         Decimal::from_str("0.01").unwrap()
     );
 
-    c.bench_function("from_snapshot_10k", |b| {
+    c.bench_function(&format!("from_snapshot_{}", SNAPSHOT_LEVELS), |b| {
         b.iter(|| {
             let _book = black_box(OrderBook::from_snapshot(black_box(snapshot.clone()), &scaler));
         })
@@ -28,19 +29,22 @@ fn bench_apply_updates(c: &mut Criterion) {
         Decimal::from_str("0.01").unwrap(),
         Decimal::from_str("0.01").unwrap()
     );
-    let mut book = OrderBook::from_snapshot(snapshot, &scaler);
 
-    let updates: Vec<DepthUpdate> = (0..UPDATE_LEVELS)
-        .map(|i| DepthUpdate::fake_update(i as u64, UPDATE_LEVELS))
+    let updates: Vec<DepthUpdate> = (0..UPDATES_PER_BATCH)
+        .map(|i| DepthUpdate::fake_update(i as u64, LEVELS_PER_UPDATE))
         .collect();
 
-    c.bench_function("apply_10k_updates", |b| {
-        b.iter(|| {
+    c.bench_function(&format!("apply_updates_{}_updates_per_batch_{}_levels_per_update", UPDATES_PER_BATCH, LEVELS_PER_UPDATE), |b| {
+    b.iter_batched_ref(
+        || OrderBook::from_snapshot(snapshot.clone(), &scaler),
+        |book| {
             for up in &updates {
                 book.apply_update(black_box(up), &scaler);
             }
-        })
-    });
+        },
+        BatchSize::SmallInput,
+    )
+});
 }
 
 fn bench_query_functions(c: &mut Criterion) {
